201099e376f818ec8d5a5dfcdd4629c24bab0f15
Rewina <zerourewina9@gmail.com>
2022-03-17 23:01:25 +0400

    More_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h";
+
+/**
+ * _isupper - checks if variable c is in uppercase
+ * RETURN: 1 if c is uppercase, 0 otherwise 
+ * 
+ */
+
+int _isupper(int c)
+{
+	if(c >= 65 && c<=90)
+	{
+		return (1);
+	}
+	return (0);
+}
