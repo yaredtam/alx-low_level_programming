beb3ef8fb1c3cfdde6215e47b9960229ded5f06f
Rewina <zerourewina9@gmail.com>
2022-03-21 22:58:04 +0400

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * swap_int - swaps two integers
- * @a: pointer pointing to the first int
- * @b: pointer pointing to the second int
+ * _puts - prints string
+ * @str: pointer pointing to the string
  * Return: void
  */
 void _puts(char *str)
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers
+ * @a: pointer pointing to the first int
+ * @b: pointer pointing to the second int
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	
+}
