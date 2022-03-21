ff021e2acee1f939a607396a6be78813bbc60201
Rewina <zerourewina9@gmail.com>
2022-03-21 21:39:47 +0400

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h";
+
+/**
+ * reset_to_98 - sets any variable to 98
+ * @n: a string pointing to the memory location to be updated
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
